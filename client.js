abfc5f919918dd2298e0f54343d630dba803549b
Your Name <you@example.com>
2021-07-06 11:59:15 +0400

    second vesion

--- client.js
+++ client.js
@@ -1,25 +1,10 @@
 const axios = require('axios');
-const names = require('./client/firstName');
-const cities = require('all-the-cities');
- 
-let a = cities.filter(city => city.name.match());
-console.log(a[10].name);
-
-let namesIndex = Math.floor(Math.random() * names.length - 1);
-console.log( names [namesIndex] );
-
-let minAge = 17;
-let maxAge = 50;
-minAge = Math.ceil(minAge);
-maxAge = Math.floor(maxAge + 1);
-let age = Math.floor(Math.random() * (maxAge - minAge) + minAge);
-
-let professionId = Math.floor(Math.random() * 16);
- 
+let personalInformation = require('./client/controler/cantroller')
+console.log(personalInformation)
 
 axios.get('http://localhost:8000/student/create', 
-{ 
-    params : {name : names[namesIndex], age : age, professionId : professionId, address : a[10].name}
+{
+    params : personalInformation
 })
 .then(function (response) { 
     console.log(response.data)
